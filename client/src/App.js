8b9f1afb177fc9ad91d8bb89c3984cfe82930071
Desiree Morimoto <dmorimoto89@gmail.com>
2023-12-05 22:50:35 -0800

    Add UserSignOut component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" element={<Courses />} />
       <Route path="/courses/:id" element={<CourseDetail />} />
       <Route path="/signin" element={<UserSignIn />} />
+      <Route path="/signout" element={<UserSignOut />} />
     </Routes>
     </>
   );
