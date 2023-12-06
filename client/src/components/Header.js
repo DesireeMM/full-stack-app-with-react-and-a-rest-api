89ba7dd2036147d57ebf342b14aa8261d3449b5e
Desiree Morimoto <dmorimoto89@gmail.com>
2023-12-05 23:04:44 -0800

    Update Header to account for signed in user

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import UserContext from "../context/UserContext";
 
 const Header = () => {
+    const { authUser } = useContext(UserContext);
+
     return (
         <div className="wrap header--flex">
             <h1 className="header--logo"><a href="/">Courses</a></h1>
             <nav>
+                { authUser === null ? 
                 <ul className="header--signedout">
-                    <li><a href="/signup">Sign Up</a></li>
-                    <li><a href="/signin">Sign In</a></li>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                    <li><Link to="/signin">Sign In</Link></li>
                 </ul>
+                :
+                <ul className="header--signedin">
+                    <span>Welcome, {authUser.firstName}!</span>
+                    <Link to="/signout">Sign Out</Link>
+                </ul>
+            }
             </nav>
         </div>
     );
