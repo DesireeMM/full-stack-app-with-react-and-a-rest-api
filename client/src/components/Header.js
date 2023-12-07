453e9bb87792123b312d47ae7ae3c9bce5fffa76
Desiree Morimoto <dmorimoto89@gmail.com>
2023-12-07 10:33:47 -0800

    Fix CSS

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,22 +7,24 @@ const Header = () => {
     const { authUser } = useContext(UserContext);
 
     return (
-        <div className="wrap header--flex">
-            <h1 className="header--logo"><a href="/">Courses</a></h1>
-            <nav>
-                { authUser === null ? 
-                <ul className="header--signedout">
-                    <li><Link to="/signup">Sign Up</Link></li>
-                    <li><Link to="/signin">Sign In</Link></li>
-                </ul>
-                :
-                <ul className="header--signedin">
-                    <span>Welcome, {authUser.firstName}! </span>
-                    <Link to="/signout">Sign Out</Link>
-                </ul>
-            }
-            </nav>
-        </div>
+        <header>
+            <div className="wrap header--flex">
+                <h1 className="header--logo"><a href="/">Courses</a></h1>
+                <nav>
+                    {authUser === null ?
+                        <ul className="header--signedout">
+                            <li><Link to="/signup">Sign Up</Link></li>
+                            <li><Link to="/signin">Sign In</Link></li>
+                        </ul>
+                        :
+                        <ul className="header--signedin">
+                            <span>Welcome, {authUser.firstName}! </span>
+                            <Link to="/signout">Sign Out</Link>
+                        </ul>
+                    }
+                </nav>
+            </div>
+        </header>
     );
 };
 
